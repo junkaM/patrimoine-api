e77bea57249b67a9e7055f0fe0067d5df395c768
junkaM <hei.mikanto.3@gmail.com>
2024-09-23 13:57:33 +0200

    init patrimoine API project

---
+++ src/main/java/com/hei/patrimoine_api/PatrimoineApiApplication.java
@@ -0,0 +1,13 @@
+package com.hei.patrimoine_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimoineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrimoineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hei/patrimoine_api/PatrimoineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hei.patrimoine_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimoineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
